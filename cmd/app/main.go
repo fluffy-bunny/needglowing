c6c9860af29a4244254377b9106b129aff5392bb
Herb <ghstahl@gmail.com>
2021-12-09 10:07:23 -0800

    using deleted dependency

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"github.com/fluffy-bunny/glowing/pkg/junk"
 	"github.com/fluffy-bunny/glowing/pkg/utils"
 )
 
 func main() {
 
-	d := utils.GenerateRandomServiceName("hello", 32)
-	println("Hello, world." + d)
+	println("Hello, world." + utils.GenerateRandomServiceName("hello", 32))
+	println("Hello, world." + junk.GenerateRandomServiceName("hello", 32))
+
 }
